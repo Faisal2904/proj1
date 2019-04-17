04a29e333c5348017bc86984fda2a89846617c92
Faisal2904 <ahmad.irfan94@gmail.com>
2019-04-17 20:42:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-yu8nq4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-yu8nq4)
---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,19 @@
+import { Component,Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: [ './server-element.component.css' ]
+})
+export class ServerElementComponent implements OnInit {
+  name = 'Angular';
+  @Input() element:{type:string, name:string, staus: string};
+
+  constructor(){
+   //console.log(this.element);
+ } 
+
+  ngOnInit(){
+   console.log(this.element);
+}
+}
